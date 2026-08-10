5a643fffddd674ed2b9dde36d17978543f23364a
Tobias Persson <tobper@gmail.com>
2026-08-10 18:11:38 +0200

    Remove obsolete anchoring support using @floating-ui

--- src/lib/components/anchor.ts
+++ src/lib/components/anchor.ts
@@ -1,18 +1,7 @@
-import { browser } from '$app/environment';
-import { get_style, toggle_root_class } from '$lib/css';
-import { device } from '$lib/device';
+import { get_style } from '$lib/css';
 import { get_element, type ElementReference } from '$lib/html';
 import { unique_id } from '$lib/unique_id';
-import { autoPlacement, autoUpdate, computePosition, offset, size } from '@floating-ui/dom';
 import type { ActionReturn } from 'svelte/action';
-
-export const anchoring_supported =
-	browser &&
-	!device.browser_name?.includes('Safari') &&
-	('anchorName' in document.documentElement.style);
-
-if (anchoring_supported)
-	toggle_root_class('supports-anchoring');
 
 export interface AnchorParameters {
 	anchor: ElementReference;
@@ -23,8 +12,6 @@ export function anchor(
 	element: HTMLElement,
 	options: AnchorParameters
 ): ActionReturn<AnchorParameters> {
-	// Remove plugin when anchoring is fully supported
-	// https://caniuse.com/?search=anchor
 	update(options);
 
 	return { update };
@@ -32,51 +19,13 @@ export function anchor(
 	function update(options: AnchorParameters) {
 		const anchor_element = get_element(options.anchor);
 
-		if (anchoring_supported) {
-			let anchor_name = get_style(anchor_element, 'anchor-name');
-
-			if (anchor_name === 'none') {
-				anchor_name = `--${unique_id()}`;
-				anchor_element.style.setProperty('anchor-name', anchor_name);
-			}
-
-			element.style.setProperty('position-anchor', anchor_name);
-			element.classList.toggle('anchor--match-width', options.match_width);
+		let anchor_name = get_style(anchor_element, 'anchor-name');
+		if (anchor_name === 'none') {
+			anchor_name = `--${unique_id()}`;
+			anchor_element.style.setProperty('anchor-name', anchor_name);
 		}
-		else {
-			const strategy = 'fixed'
-			const middleware = [
-				autoPlacement({
-					allowedPlacements: ['top-start', 'bottom-start', 'top-end', 'bottom-end']
-				}),
-				offset(6),
-				options.match_width && size({
-					apply({ rects, elements }) {
-						Object.assign(elements.floating.style, {
-							'min-width': `max(var(--menu__min-width), ${rects.reference.width}px)`
-						})
-					}
-				})
-			];
 
-			updatePosition();
-
-			return autoUpdate(anchor_element, element, updatePosition)
-
-			async function updatePosition() {
-				const result = await computePosition(
-					anchor_element,
-					element,
-					{
-						middleware,
-						strategy
-					}
-				);
-
-				element.style.position = strategy;
-				element.style.left = `${result.x}px`;
-				element.style.top = `${result.y}px`;
-			}
-		}
+		element.style.setProperty('position-anchor', anchor_name);
+		element.classList.toggle('anchor--match-width', options.match_width);
 	}
 }
